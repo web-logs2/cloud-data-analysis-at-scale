6dfa23fe5572dc0036a6fb5370733b2c9ccd0dad
ziyuan wang <zw257@duke.edu>
2023-01-25 22:33:29 -0500

    add project 1: build container

---
+++ project_1/hello_world/main.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello I like to make AI Apps'
+
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
