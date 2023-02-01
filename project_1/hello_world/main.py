c4b95dcae76ca8c90d04f0aa24f8988c32ae1df7
ziyuan wang <zw257@duke.edu>
2023-01-31 23:47:18 -0500

    1

--- project_1/hello_world/main.py
+++ project_1/hello_world/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import jsonify
 import pandas as pd
-import wikipedia
+
 
 
 app = Flask(__name__)
@@ -29,20 +29,6 @@ def pandas_sugar():
     df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
     return jsonify(df.to_dict())
 
-# @app.route('/wikipedia/<company>')
-# def wikipedia_route(company):
-
-#     # Imports the Google Cloud client library
-#     from google.cloud import language
-#     result = wikipedia.summary(company, sentences=10)
-
-#     client = language.LanguageServiceClient()
-#     document = language.Document(
-#         content=result,
-#         type_=language.Document.Type.PLAIN_TEXT)
-#     encoding_type = language.EncodingType.UTF8
-#     entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
-#     return str(entities)
 
 
 if __name__ == '__main__':
