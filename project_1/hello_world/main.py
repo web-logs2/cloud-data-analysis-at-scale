ba719fd02dea2694b68ee1bb5814376b7c64d7d6
branwn <50729260+branwn@users.noreply.github.com>
2023-01-31 20:39:31 -0500

    Update main.py

--- project_1/hello_world/main.py
+++ project_1/hello_world/main.py
@@ -1,6 +1,8 @@
-
 from flask import Flask
 from flask import jsonify
+import pandas as pd
+import wikipedia
+
 
 app = Flask(__name__)
 
@@ -22,6 +24,26 @@ def html():
     <p>Hello</p>
     <p><b>World</b></p>
     """
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+
+    # Imports the Google Cloud client library
+    from google.cloud import language
+    result = wikipedia.summary(company, sentences=10)
+
+    client = language.LanguageServiceClient()
+    document = language.Document(
+        content=result,
+        type_=language.Document.Type.PLAIN_TEXT)
+    encoding_type = language.EncodingType.UTF8
+    entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
+    return str(entities)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
