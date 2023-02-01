d18dae427b740bbb269be4fa4c04c5ecfeb1c4a9
branwn <50729260+branwn@users.noreply.github.com>
2023-02-01 08:26:11 +0800

    Update main.py

--- project_1/hello_world/main.py
+++ project_1/hello_world/main.py
@@ -14,5 +14,14 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
+@app.route('/html')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
