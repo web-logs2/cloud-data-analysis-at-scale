23e494f591bf072291cac46c79721b1bb97b57fa
ziyuan wang <zw257@duke.edu>
2023-01-31 22:29:39 -0500

    a

--- project_1/hello_world/main.py
+++ project_1/hello_world/main.py
@@ -29,20 +29,20 @@ def pandas_sugar():
     df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
     return jsonify(df.to_dict())
 
-@app.route('/wikipedia/<company>')
-def wikipedia_route(company):
+# @app.route('/wikipedia/<company>')
+# def wikipedia_route(company):
 
-    # Imports the Google Cloud client library
-    from google.cloud import language
-    result = wikipedia.summary(company, sentences=10)
+#     # Imports the Google Cloud client library
+#     from google.cloud import language
+#     result = wikipedia.summary(company, sentences=10)
 
-    client = language.LanguageServiceClient()
-    document = language.Document(
-        content=result,
-        type_=language.Document.Type.PLAIN_TEXT)
-    encoding_type = language.EncodingType.UTF8
-    entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
-    return str(entities)
+#     client = language.LanguageServiceClient()
+#     document = language.Document(
+#         content=result,
+#         type_=language.Document.Type.PLAIN_TEXT)
+#     encoding_type = language.EncodingType.UTF8
+#     entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
+#     return str(entities)
 
 
 if __name__ == '__main__':
